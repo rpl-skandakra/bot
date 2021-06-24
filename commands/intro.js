66515f7b48b742567b6dd612146c5cf9763807a8
hendraaagil <hendraaagil@gmail.com>
2021-06-24 10:52:57 +0700

    Change prefix & addition on intro commands

--- commands/intro.js
+++ commands/intro.js
@@ -2,26 +2,38 @@ module.exports = {
   name: 'intro',
   description: 'To validate user introduction after joining server',
   execute: (message) => {
-    if (
-      /Nama :/.test(message.content) &&
-      /Kelas :/.test(message.content) &&
-      /Alamat :/.test(message.content) &&
-      /Hobi :/.test(message.content)
-    ) {
-      const roleMember = message.guild.roles.cache.find(
-        (role) => role.id === process.env.ROLE_MEMBER_ID
-      );
-      const user = message.guild.members.cache.find(
-        (member) => member.id === message.author.id
-      );
+    const chRole = message.guild.channels.cache.find(
+      (channel) => channel.id === process.env.CH_ROLE_ID
+    );
+    const roleMember = message.guild.roles.cache.find(
+      (role) => role.id === process.env.ROLE_MEMBER_ID
+    );
+    const user = message.guild.members.cache.find(
+      (member) => member.id === message.author.id
+    );
+    const isMember = user.roles.cache.find(
+      (role) => role.id === process.env.ROLE_MEMBER_ID
+    );
 
-      user.roles.add(roleMember);
-      message.reply(
-        `Selamat kamu sudah resmi menjadi **${roleMember.name}** dari **${message.guild.name}**`
-      );
+    if (!isMember) {
+      if (
+        /Nama :/.test(message.content) &&
+        /Kelas :/.test(message.content) &&
+        /Alamat :/.test(message.content) &&
+        /Hobi :/.test(message.content)
+      ) {
+        user.roles.add(roleMember);
+        message.reply(
+          `Selamat kamu sudah resmi menjadi **${roleMember.name}** dari **${message.guild.name}**.\nJangan lupa pilih role terlebih dahulu di ${chRole}!`
+        );
+      } else {
+        message.reply(
+          'Silahkan masukkan format perkenalan dengan benar!\n> Nama : \n> Kelas : \n> Alamat : \n> Hobi :'
+        );
+      }
     } else {
       message.reply(
-        'Silahkan masukkan format perkenalan dengan benar!\n> Nama : \n> Kelas : \n> Alamat : \n> Hobi :'
+        `Kamu sudah menjadi ${roleMember.name}, tidak perlu mengirim pesan lagi di channel ini.`
       );
     }
   },
