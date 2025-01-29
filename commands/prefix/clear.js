8738ac40e79c3e500399745e02242d4cdfdb5626
hendraaagil <hendraaagil@gmail.com>
2025-01-29 22:30:46 +0700

    refactor: cleaner code

--- commands/prefix/clear.js
+++ commands/prefix/clear.js
@@ -3,20 +3,18 @@ const { ROLE_ADMIN_ID } = process.env;
 const clearMessage = (message, text) => {
   const isAdmin = message.member.roles.cache.find((role) => role.id === ROLE_ADMIN_ID);
   const adminName = message.guild.roles.cache.find((role) => role.id === ROLE_ADMIN_ID);
-
-  if (isAdmin) {
-    if (text[1]) {
-      message.channel.bulkDelete(text[1]);
-      message.channel.send(`Berhasil menghapus ${text[1]} pesan 🔥`);
-      setTimeout(() => {
-        message.channel.bulkDelete(1);
-      }, 3000);
-    } else {
-      message.reply('Masukkan jumlah pesan yang ingin dihapus!');
-    }
-  } else {
-    message.reply(`Mohon maaf kamu bukan ${adminName} di server ini.`);
+  if (!isAdmin) {
+    return message.reply(`Mohon maaf kamu bukan ${adminName} di server ini.`);
   }
+  if (!text[1]) {
+    return message.reply('Masukkan jumlah pesan yang ingin dihapus!');
+  }
+
+  message.channel.bulkDelete(text[1]);
+  message.channel.send(`Berhasil menghapus ${text[1]} pesan 🔥`);
+  setTimeout(() => {
+    message.channel.bulkDelete(1);
+  }, 3000);
 };
 
 module.exports = { clearMessage };
